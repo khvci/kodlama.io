3da4d9fad32f1503fe8b217a63cdeced75180bf7
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-03-24 18:40:47 +0300

    Create ProductService.java

---
+++ src/main/java/khvci/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,11 @@
+package khvci.northwind.business.abstracts;
+
+import khvci.northwind.entities.concretes.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAll();
+
+
+}
